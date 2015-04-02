3beef677b8101fa8955a445fbdad13f6a5ae8d8a
vcmouli <chandramouli.vaidyanathan@gmail.com>
2015-04-02 12:29:12 +0530

    Reorganized folder structure for dataset to allow for 10 million dataset

--- src/main/java/com/jda/recommendation/DefaultRecommendationEngine.java
+++ src/main/java/com/jda/recommendation/DefaultRecommendationEngine.java
@@ -22,22 +22,21 @@ import com.jda.util.FileUtil;
 
 public class DefaultRecommendationEngine implements RecommendationEngine {
 	private static Map<String, String> moviesMap = new HashMap<String, String>();
-	private static int NUMBER_OF_RECOMMENDATIONS = 10;
-	private static int USER_ID = 716;
 	private static final Logger logger = LoggerFactory.getLogger(DefaultRecommendationEngine.class);
+	private static final String ONE_MILLION_DATASET_PATH = "dataset/01-million";
+	private static final String DATASET_PATH = ONE_MILLION_DATASET_PATH;
+	
+	private static final int DEFAULT_NUMBER_OF_RECOMMENDATIONS = 10;
+	private static int USER_ID = 716;
 	
 	static {
-		init();
-	}
-	
-	static void init() {
 		loadMovies();
 	}
-
+	
 	private static void loadMovies() {
 		FileUtil obj = new FileUtil();
-		String movies = "dataset/movies.txt";
-		moviesMap = obj.load(movies, "\\|");
+		String moviesFilePath = DATASET_PATH + "/movies.txt";
+		moviesMap = obj.load(moviesFilePath, "\\|");
 		logger.info("movies count: {}", moviesMap.size());
 	}
 
@@ -46,27 +45,13 @@ public class DefaultRecommendationEngine implements RecommendationEngine {
 		recoEngine.recommendForUser(USER_ID);
 	}
 
-	private static List<RecommendedItem> recommend(DataModel model, int userid)
-			throws Exception {
-		//UserSimilarity similarity = new TanimotoCoefficientSimilarity(model);
-		//UserSimilarity similarity = new LogLikelihoodSimilarity(model);
-		UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
-		UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
-
-		//TODO: can have user-based + item-based recommendation
-		UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
-
-		List<RecommendedItem> recommendations = recommender.recommend(userid, NUMBER_OF_RECOMMENDATIONS);
-		return recommendations;
-	}
-
 	@Override
 	public String recommendForUser(int userId) throws Exception {
 		long startTime = System.currentTimeMillis();
 		String movieName = "";
 		List<String> output = new ArrayList<String>();
 		long movieId = 0;
-		DataModel model = new FileDataModel(new File("dataset/ratings.txt"));
+		DataModel model = new FileDataModel(new File(DATASET_PATH + "/ratings.txt"));
 		logger.info("*** movie lens recommendation ****");
 		List<RecommendedItem> recommendations = recommend(model, userId);
 		for (RecommendedItem recommendation : recommendations) {
@@ -78,4 +63,19 @@ public class DefaultRecommendationEngine implements RecommendationEngine {
 		logger.info("Time taken in ms: {}", (endTime - startTime));
 		return output.toString();
 	}
+	
+	private static List<RecommendedItem> recommend(DataModel model, int userid)
+			throws Exception {
+		//UserSimilarity similarity = new TanimotoCoefficientSimilarity(model);
+		//UserSimilarity similarity = new LogLikelihoodSimilarity(model);
+		UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+		UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+
+		//TODO: can have user-based + item-based recommendation
+		UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+
+		List<RecommendedItem> recommendations = recommender.recommend(userid, DEFAULT_NUMBER_OF_RECOMMENDATIONS);
+		return recommendations;
+	}
+
 }
