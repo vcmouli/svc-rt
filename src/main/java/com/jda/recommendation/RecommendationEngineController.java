5bcdaa50ffe6377eaf7d4c0f11536abef3111102
vcmouli <chandramouli.vaidyanathan@gmail.com>
2015-04-03 11:06:54 +0530

    New method exposed to get ratings for user

--- src/main/java/com/jda/recommendation/RecommendationEngineController.java
+++ src/main/java/com/jda/recommendation/RecommendationEngineController.java
@@ -1,10 +1,16 @@
 package com.jda.recommendation;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/recommendation")
 public class RecommendationEngineController {
-	
+	@RequestMapping(method = RequestMethod.GET, value = "{userid}")
+	public String getRecommendationForUser(@PathVariable int userid) throws Exception {
+		RecommendationEngine recoEngine = new DefaultRecommendationEngine();
+		return recoEngine.recommendForUser(userid);
+	}
 }
